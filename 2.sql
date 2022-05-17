be78f742c95edb4b6806dc892e7bbefd29e2df1b
m3678jk <94804170+m3678jk@users.noreply.github.com>
2022-05-17 23:35:26 +0200

    Add files via upload

--- 2.sql
+++ 2.sql
@@ -1,19 +1,15 @@
-	   
-	select project_developer.id_project as id_pr, developers.salery as sum
+   
+	select project_developer.id_project as id_pr, sum(developers.salery) as sum_of_salery
 	from developers 
-    join project_developer on developers.id = project_developer.id_developer 
+	join project_developer on developers.id = project_developer.id_developer 
 	group by id_pr
-	having sum = (
-		select max(sum1) as max_cost
-		from (  
-			select project_developer.id_project as id_pr1, developers.salery as sum1
-			from developers 
-			join project_developer on developers.id = project_developer.id_developer 
-			group by id_pr1
-			order by count(sum1) 
-    ) as a
-	);
+	order by sum_of_salery desc
+    limit 1
+	;
     
+    
+   
+   
     
    
    
