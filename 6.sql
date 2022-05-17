ddae2c3e508c1ff40fae64cc53a3088acea559b0
m3678jk <94804170+m3678jk@users.noreply.github.com>
2022-05-17 23:53:32 +0200

    Add files via upload

--- 6.sql
+++ 6.sql
@@ -1,16 +1,11 @@
-select avg(salery) avg_salery
-from (
-	select project_developer.id_project, developers.salery
+	select project_developer.id_project, avg(developers.salery)
 	from project_developer 
-	inner join developers on developers.id = project_developer.id_developer
+	join developers on developers.id = project_developer.id_developer
+	group by project_developer.id_project
 	having project_developer.id_project = (
 		select id_project
 		from ( 
-			select  id_project, cost
+			select  id_project, min(cost) as min_sum
 			from projects
-			having cost = (
-				select min(cost) as cost_min
-				from projects
-			))as al2
-		)) as al1
-;
+		)as al
+    );
